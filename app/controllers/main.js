08f14e6d0c57e4976f7e9644a20fd9b43e49663e
Quang Thai-Dev <thaivq.dev@gmail.com>
2021-06-28 20:41:23 +0700

    final

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -4,6 +4,70 @@ function getEl(id) {
     return document.getElementById(id);
 }
 
+var layDanhSachSP = function() {
+    sanPhamService.layDanhSachSP().then((result) => {
+        // Thành công
+        // console.log(result.data);
+        // render ra table
+        renderTable(result.data);
+        setLocalStorage(result.data);
+    }).catch((err) => {
+        // Thất bại
+        console.log(err);
+    });
+}
+layDanhSachSP();
+
+var themSP = function() {
+    // Lấy thông tin sản phẩm
+    var tenSP = getEl('TenSP').value;
+    var gia = getEl('GiaSP').value;
+    var hinhAnh = getEl('HinhSP').value;
+    var moTa = getEl('moTa').value;
+
+    // Khởi tạo đối tượng sản phẩm từ lớp đối tượng sản phẩm
+    var sp = new SanPham(tenSP, gia, hinhAnh, moTa);
+    console.log(sp);
+    // Thêm sản phẩm
+    sanPhamService.themSanPham(sp).then((result) => {
+        layDanhSachSP();
+    }).catch((err) => {
+        console.log(err);
+    });
+}
+
+var modalFooter = document.querySelector('.modal-footer');
+var modalTitle = document.querySelector('.modal-title');
+getEl('btnThemSP').addEventListener('click', function() {
+    modalTitle.innerHTML = 'Thêm sản phẩm';
+    modalFooter.innerHTML = `<buttuon class ="btn btn-success" onclick = "themSP()" > Thêm sản phẩm </button>`
+})
+
+var xoaSP = function(id) {
+    sanPhamService.xoaSanPham(id).then(function(result) {
+        layDanhSachSP();
+    }).catch(function(err) {
+        console.log(err);
+    });
+}
+
+var xemSP = function(id) {
+
+    sanPhamService.xemSamPham(id).then(function(result) {
+        console.log(result);
+        getEl('btnThemSP').click();
+        // $('#myModal').modal('show');
+        getEl('TenSP').value = result.data.tenSP;
+        getEl('GiaSP').value = result.data.gia;
+        getEl('HinhSP').value = result.data.hinhAnh;
+        getEl('moTa').value = result.data.moTa;
+        modalTitle.innerHTML = 'Cập nhật sản phẩm';
+        modalFooter.innerHTML = `<buttuon class ="btn btn-success"> Câp nhật sản phẩm </button>`
+    }).catch(function(err) {
+        console.log(err);
+    });
+}
+
 function renderTable(mangSP) {
     var content = '';
     mangSP.map(function(sp, index) {
@@ -17,8 +81,8 @@ function renderTable(mangSP) {
                 </td>
                 <td>${sp.moTa}</td>
                 <td>
-                    <button class="btn btn-info">Xem</button>
-                    <button class="btn btn-danger">Xóa</button>
+                    <button class="btn btn-info" onclick = "xemSP('${sp.id}')" >Xem</button>
+                    <button class="btn btn-danger" onclick="xoaSP('${sp.id}')">Xóa</button>
                 </td>
             </tr>
         `
@@ -26,15 +90,14 @@ function renderTable(mangSP) {
     getEl('tblDanhSachSP').innerHTML = content;
 }
 
-var layDanhSachSP = function() {
-    sanPhamService.layDanhSachSP().then((result) => {
-        // Thành công
-        // console.log(result.data);
-        // render ra table
-        renderTable(result.data);
-    }).catch((err) => {
-        // Thất bại
-        console.log(err);
-    });;
+
+
+function setLocalStorage(dssp) {
+    localStorage.setItem('DSSP', JSON.stringify(dssp));
 }
-layDanhSachSP();
+
+function getLocalStorage() {
+    if (localStorage.getItem('DSSP')) {
+        return JSON.parse(localStorage.getItem('DSSP'));
+    }
+}
--- app/services/SanPhamService.js
+++ app/services/SanPhamService.js
@@ -8,4 +8,30 @@ function SanPhamService() {
         });
         return promise;
     }
+
+    this.themSanPham = function(sp) {
+        // POST: Thêm mới dữ liệu
+        // data: dữ liệu cần thêm vào database
+        return axios({
+            url: 'https://60d5dbf7943aa60017768c50.mockapi.io/products',
+            method: 'POST',
+            data: sp,
+        });
+    }
+
+    this.xoaSanPham = function(id) {
+        // DELETE: xóa data
+        return axios({
+            url: `https://60d5dbf7943aa60017768c50.mockapi.io/products/${id}`,
+            method: 'DELETE',
+        })
+    }
+
+    this.xemSamPham = function(id) {
+        // GET: Lấy data sản phẩm dựa vào id
+        return axios({
+            url: `https://60d5dbf7943aa60017768c50.mockapi.io/products/${id}`,
+            method: 'GET',
+        })
+    }
 }
